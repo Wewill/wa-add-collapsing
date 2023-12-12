86d40d7fd9ddf0d915160d2425cfa4f746d7ce3d
Wilhem Arnoldy <contact@wilhemarnoldy.fr>
2023-12-12 17:05:57 +0100

    Add settongs page to plugin

--- wa-add-collapsing.php
+++ wa-add-collapsing.php
@@ -8,7 +8,7 @@ Version: 1.0
  * Author URI:        https://www.wilhemarnoldy.fr
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       wa-rsfp
+ * Text Domain:       wa-adco
  * Domain Path:       /languages
 */
 
@@ -34,8 +34,10 @@ if ( !class_exists( 'Add_Collapsing_Tax' ) ) :
         /**
          * @var Add_Collapsing_Tax Array of taxonomies to implement folded view
          */
-        protected $taxonomies = array( 'category', 'production', 'thematic' );
-        protected $posts = array( 'page' );
+        protected $default_taxonomies = array();
+        protected $default_posts = array();
+        protected $taxonomies = array();
+        protected $posts = array();
 
         /**
          * Main Add_Collapsing_Tax Instance
@@ -57,6 +59,12 @@ if ( !class_exists( 'Add_Collapsing_Tax' ) ) :
          */
         public function __construct() {
             add_action( 'admin_enqueue_scripts', array( $this, 'load_scripts' ) );
+
+            // Add a page in settings
+            add_filter( 'mb_settings_pages', array( $this, 'add_setting_page' ) );
+            add_filter( 'rwmb_meta_boxes',  array( $this, 'add_custom_fields_to_setting_page' ) );
+
+            // @TODO language
         }
 
         /*
@@ -64,6 +72,12 @@ if ( !class_exists( 'Add_Collapsing_Tax' ) ) :
          */
         public function load_scripts() {
             global $pagenow;
+
+            // Get settings
+            $this->taxonomies   = array_unique(array_merge($this->default_taxonomies, $this->get_taxonomies_from_setting_page()), SORT_REGULAR);
+            $this->posts        = array_unique(array_merge($this->default_posts, $this->get_posts_from_setting_page()), SORT_REGULAR);
+
+            // Init scripts & styles
             if ( 'edit-tags.php' === $pagenow && isset( $_GET['taxonomy'] ) && array_search( $_GET['taxonomy'], $this->taxonomies) !== false && !isset( $_GET['s'] ) && !isset( $_GET['orderby'] ) ) {
                 wp_enqueue_style( 'add-collapsing-tax', plugin_dir_url( __FILE__ ) . '/css/add-collapsing-tax.css', array(), '1.0' );
                 wp_enqueue_script( 'add-collapsing-tax', plugin_dir_url( __FILE__ ) . '/js/add-collapsing-tax.js', array('jquery'), '1.0', true );
@@ -72,6 +86,73 @@ if ( !class_exists( 'Add_Collapsing_Tax' ) ) :
                 wp_enqueue_style( 'add-collapsing-post', plugin_dir_url( __FILE__ ) . '/css/add-collapsing-post.css', array(), '1.0' );
                 wp_enqueue_script( 'add-collapsing-post', plugin_dir_url( __FILE__ ) . '/js/add-collapsing-post.js', array('jquery'), '1.0', true );
             }
+        }
+
+        public function add_setting_page( $settings_pages ) {
+            $settings_pages[] = [
+                'menu_title' => __( 'Collapsing Taxonomies', 'wa-rsfp' ),
+                'id'         => 'collapsing-taxonomies',
+                'parent'     => 'options-general.php',
+                'class'      => 'custom_css',
+                'style'      => 'no-boxes',
+                // 'message'    => __( 'Custom message', 'wa-rsfp' ), // Saved custom message
+                'customizer' => true,
+                'icon_url'   => 'dashicons-admin-generic',
+            ];
+        
+            return $settings_pages;
+        }
+
+        public function add_custom_fields_to_setting_page( $meta_boxes ) {
+            $prefix = 'waadco_';
+        
+            $meta_boxes[] = [
+                'id'             => 'collapsing-taxonomies-fields',
+                'settings_pages' => ['collapsing-taxonomies'],
+                'fields'         => [
+                    [
+                        'name'            => __( 'Allowed taxonomy.ies', 'wa-adco' ),
+                        'id'              => $prefix . 'allowed_taxonomy',
+                        'type'            => 'checkbox_list',
+                        'inline'          => true,
+                        'select_all_none' => true,
+                        'options'         => $this->taxonomies_options_callback(),
+                    ],
+                ],
+            ];
+
+            $meta_boxes[] = [
+                'id'             => 'collapsing-posts-fields',
+                'settings_pages' => ['collapsing-taxonomies'],
+                'fields'         => [
+                    [
+                        'name'            => __( 'Allowed post.s', 'wa-adco' ),
+                        'id'              => $prefix . 'allowed_post',
+                        'type'            => 'checkbox_list',
+                        'inline'          => true,
+                        'select_all_none' => true,
+                        'options'         => $this->posts_options_callback(),
+                    ],
+                ],
+            ];
+        
+            return $meta_boxes;
+        }
+
+        public function taxonomies_options_callback() {
+            return get_taxonomies();
+        }
+        
+        public function posts_options_callback() {
+            return get_post_types();
+        }
+
+        public function get_taxonomies_from_setting_page() {
+            return rwmb_meta( 'waadco_allowed_taxonomy', [ 'object_type' => 'setting' ], 'collapsing-taxonomies' );
+        }
+
+        public function get_posts_from_setting_page() {
+            return rwmb_meta( 'waadco_allowed_post', [ 'object_type' => 'setting' ], 'collapsing-taxonomies' );
         }
 
     }
